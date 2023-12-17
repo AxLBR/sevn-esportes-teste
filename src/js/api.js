0b5d545eb7af1653f36fe549b79d8dadc8bf5dbf
Gabriel Castro <gabrieltoby@gmail.com>
2023-12-17 16:40:33 -0300

    implementação dados API

--- src/js/api.js
+++ src/js/api.js
@@ -1,33 +1,14 @@
-let round = 1;
+//Variáveis globais
+let round = 1; //Número do round atual
+let newResult = ''; //Dados coletados da API
 
-//Cria a estrutura do card com os dados
-async function createData(result, index){
-console.log(result, index)
+init();
 
-result.map((item, index) => {
-    let card = document.querySelector('.cardModelContainer').cloneNode(true);
-    let newTeamAwayId = item.team_away_id.slice(-1);
-    let newTeamHomeId = item.team_home_id.slice(-1);
-    
-    console.log(item.team_away_name + ' x ' + item.team_home_name)
+//Inicializa o processo de requisição dos resultados dos jogos
+async function init(){
+    const results = await getResultsAPI();
 
-    card.setAttribute('data-key', index);
-    card.querySelector('.team1 img').src = `./src/images/team_shield_${newTeamAwayId}.png`;
-    card.querySelector('.team1 p').textContent  = item.team_away_name;
-    card.querySelector('.score p:first-of-type').textContent  = item.team_away_score;
-    card.querySelector('.score p:last-of-type').textContent  = item.team_home_score;
-    card.querySelector('.team2 p').textContent  = item.team_home_name;
-    card.querySelector('.team2 img').src = `./src/images/team_shield_${newTeamHomeId}.png`;
-
-    //Preenche os videos na página
-    document.querySelector('.cardBody').append(card);
-})
-}
-
-//Cria os itens do card com os resultados
-async function createCardItens(results){
-
-    createData(results.data[round-1].games, round-1);
+    createCardItens(results);
 }
 
 //Faz a requisição dos dados via Axios
@@ -45,11 +26,63 @@ async function getResultsAPI(){
     }
 }
 
-//Inicializa o processo de requisição dos resultados dos jogos
-async function init(){
-    const results = await getResultsAPI();
-    console.log(results);
-    createCardItens(results);
+//Passa os dados coletados para a variável global
+async function createCardItens(results){
+    newResult = results;
+    createData(results.data[round-1].games, round-1);
 }
 
-init();
+//Cria a estrutura do card com os dados
+async function createData(result, index){
+
+result.map((item, index) => {
+    let card = document.querySelector('.cardModelContainer').cloneNode(true); //Clona o modelo de card
+    let newTeamAwayId = item.team_away_id.slice(-1);
+    let newTeamHomeId = item.team_home_id.slice(-1);
+
+    //Preenche os dados no modelo
+    card.setAttribute('data-key', index);
+    document.querySelector('.titles p:last-of-type').textContent  = 'RODADA ' + round;
+    card.querySelector('.team1 img').src = `./src/images/team_shield_${newTeamAwayId}.png`;
+    card.querySelector('.team1 p').textContent  = item.team_away_name;
+    card.querySelector('.score p:first-of-type').textContent  = item.team_away_score;
+    card.querySelector('.score p:last-of-type').textContent  = item.team_home_score;
+    card.querySelector('.team2 p').textContent  = item.team_home_name;
+    card.querySelector('.team2 img').src = `./src/images/team_shield_${newTeamHomeId}.png`;
+
+    //Preenche os dados na página
+    document.querySelector('.cardBody').append(card);
+})
+}
+
+//Botões do slider
+const prevButton = document.querySelector(".btnLeft");
+const nextButton = document.querySelector(".btnRight");
+
+prevButton.addEventListener("click", () => {
+    if (round > 1){
+        round--;
+        nextButton.classList.remove("btnOpacity");
+    }
+
+    round == 1 ? prevButton.classList.add("btnOpacity") : '';
+    
+    document.querySelector('.cardBody').innerHTML = '';
+    document.querySelector('.titles p:last-of-type').textContent  = 'RODADA ' + round;
+
+    createData(newResult.data[round-1].games, round-1);
+});
+
+nextButton.addEventListener("click", () => {
+    if (round < newResult.data.length){
+        round++;
+        prevButton.classList.remove("btnOpacity");
+    }
+
+    round == newResult.data.length ?  nextButton.classList.add("btnOpacity") : '';
+
+    document.querySelector('.cardBody').innerHTML = '';
+    document.querySelector('.titles p:last-of-type').textContent  = 'RODADA ' + round;
+
+    createData(newResult.data[round-1].games, round-1);
+});
